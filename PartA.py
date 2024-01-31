91d1de8ee159526b49e8565e442cf50155244c55
B00137752 Sean Amistoso <B00137752@mytudublin.ie>
2024-01-31 12:07:09 +0000

    added printsteps

---
+++ PartA.py
@@ -0,0 +1,12 @@
+#Sean Amistoso
+#31/01/2024
+#A Program to Print Steps 1 to 9 to the screen
+
+def PrintSteps() : 
+    print("Step 4")
+    print("Step 5")
+    print("Step 6")
+    
+PrintSteps()
+
+
